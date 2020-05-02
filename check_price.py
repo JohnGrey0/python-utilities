283227a43f965fa3f0dda1e1a011bcc21133ad4e
johnhhancock <hancockherbertjohn@gmail.com>
2020-05-02 02:13:14 -0400

    added seperate programs for cron
    cron dislikes argparse

---
+++ check_price.py
@@ -0,0 +1,4 @@
+from helpers.generic_helper_methods import price_checker
+
+if __name__ == '__main__':
+    price_checker()
---
+++ test_speed.py
@@ -0,0 +1,6 @@
+from helpers.generic_helper_methods import tweet_speed_to_comcast
+
+if __name__ == '__main__':
+    tweet_speed_to_comcast(platform='pi')
+        
+    
